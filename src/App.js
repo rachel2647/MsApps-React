96d0f9ceabee1ad7a292b39c11d751e4b34cb7be
rachel2647 <113825822+rachel2647@users.noreply.github.com>
2023-12-24 12:46:10 +0200

    fix commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import { Provider } from 'react-redux'; // Importing the Provider component from react-redux
-import store from './Redux/store'; // Importing the Redux store
-import ToggleOnIcon from '@mui/icons-material/ToggleOn'; // Importing the ToggleOnIcon component from @mui/icons-material
-import './App.css'; // Importing the CSS file for the App component
-import ChooseCategory from './Components/chooseCategory'; // Importing the ChooseCategory component
-import { useState } from 'react'; // Importing the useState hook from react
-
+import { Provider } from 'react-redux';
+import store from './Redux/store';
+import ToggleOnIcon from '@mui/icons-material/ToggleOn';
+import './App.css'; 
+import ChooseCategory from './Components/chooseCategory'; 
+import { useState } from 'react';
 function App() {
-  const [isClicked, setIsClicked] = useState(false); // Setting up state using the useState hook - a boolean indicating if the button was clicked or not
+  const [isClicked, setIsClicked] = useState(false); 
   
-  const handleClick = () => { // Event handler function for the button click
-    setIsClicked(true); // Updating the state to indicate that the button was clicked
+  const handleClick = () => { 
+    setIsClicked(true); 
   }
   
   return (
     <div>
-      <Provider store={store}> {/* Wrapping the application with the Provider component and providing the Redux store */}
-        {isClicked ? <ChooseCategory /> : //Conditional rendering based on the isClicked state
+      <Provider store={store}> 
+        {isClicked ? <ChooseCategory /> : 
           <div id="app-div">
             <button onClick={handleClick}>
               Click me to select category
